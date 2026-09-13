d9fd76bed218dd1baaf05c021bf25c68074753d9
Leo Li <cheerleaderleo@outlook.com>
2026-09-13 18:20:12 -0400

    Revert "Keep folder title formatting outside the titlebar view builder"
    This reverts commit fe869a407c6cfa4a48e260c087534e599e3146f1.

--- Sources/ContentView.swift
+++ Sources/ContentView.swift
@@ -2181,7 +2181,7 @@ struct ContentView: View {
                             .padding(.leading, 0)
                     }
 
-                    Text(titlebarLocationText)
+                    Text(focusedDirectory.map { ($0 as NSString).lastPathComponent } ?? titlebarText)
                         .cmuxFont(size: 12, weight: .medium)
                         .foregroundColor(fakeTitlebarTextColor(appearance: appearance))
                         .lineLimit(1)
@@ -2342,11 +2342,6 @@ struct ContentView: View {
                 hostingSafeAreaTop = nextSafeAreaTop
             }
         }
-    }
-
-    private var titlebarLocationText: String {
-        guard let directory = focusedDirectory else { return titlebarText }
-        return (directory as NSString).lastPathComponent
     }
 
     @MainActor private func updateTitlebarText() {
